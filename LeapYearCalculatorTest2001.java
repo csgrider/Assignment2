2baedcec83dfa520ba6217be8c3c239946bc49be
csgrider <54371179+csgrider@users.noreply.github.com>
2019-08-29 22:08:18 -0400

    Create LeapYearCalculatorTest2001.java

---
+++ LeapYearCalculatorTest2001.java
@@ -0,0 +1,13 @@
+package edu.bsu.cs222;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LeapYearCalculatorTest2001 {
+    @Test
+    public void test2001IsLeapYear(){
+        LeapYearCalculator leapYear = new LeapYearCalculator();
+        boolean result = leapYear.isLeapYear(2001);
+        Assert.assertTrue(result);
+    }
+}
