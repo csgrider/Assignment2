dcc5e51dd19395f2080b02aebedee82b527c0cd6
csgrider <54371179+csgrider@users.noreply.github.com>
2019-09-09 18:50:29 -0400

    Update LeapYearCalculator.java

--- LeapYearCalculator.java
+++ LeapYearCalculator.java
@@ -2,6 +2,21 @@ package edu.bsu.cs222;
 
 public class LeapYearCalculator {
     public boolean isLeapYear(int year) {
-        return true;
+        if(year % 400 == 0){
+            if(year % 100 != 0){
+                if(year % 4 == 0){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
     }
 }
